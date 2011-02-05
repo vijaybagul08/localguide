d4f159b2dcbc6808265b68a29d344e50c00ab6ff
smanikandan14 <smanikandan14@a7035fdc-282f-eb65-315d-6cc58e1ab02f>
2011-02-05 15:23:50 +0000

    Adding custom mapsItemizedOverlay class, getDiectionList class to fetch routes between two places using google maps api

---
+++ trunk/src/com/android/localguide/GetDirectionsList.java
@@ -0,0 +1,126 @@
+package com.android.localguide;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GetDirectionsList {
+	
+	public interface SearchResultCallBack {
+		public void OnSearchCompleted(int result);
+		public static final int SUCCESS 	= 1;
+		public static final int NETWORK_FAILURE		= 2;
+	}
+	
+	public class DirectionItem
+	{
+		
+		double latitude;
+		double longitude;
+		int duration;
+		String distance;
+		String instructions;
+	}
+
+	private ArrayList<DirectionItem> DirectionItemList;
+	private String result;
+	private String startLocation;
+	private String destination;
+	private SearchResultCallBack mCB;
+	
+	GetDirectionsList(String start, String dest,SearchResultCallBack aCB)
+	{
+		startLocation = start;
+		destination = dest;
+		mCB = aCB;
+	}
+	
+	public void searchRoutes()
+	{
+		  	HttpClient client = new DefaultHttpClient();
+			String query = "http://maps.googleapis.com/maps/api/directions/json?origin="+startLocation+"&destination="+destination+"&sensor=false";
+
+			try {
+				URL url = new URL(query);
+				URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+				HttpGet request = new HttpGet(uri);
+				HttpParams httpParameters = new BasicHttpParams();
+				// Set the timeout in milliseconds until a connection is established.
+				int timeoutConnection = 3000;
+				HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+				// Set the default socket timeout (SO_TIMEOUT) 
+				// in milliseconds which is the timeout for waiting for data.
+				int timeoutSocket = 5000;
+				HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+				
+				HttpResponse response = client.execute(request);
+				Userrequest(response);
+			}catch (URISyntaxException e){
+				
+			}
+			catch(Exception ex){
+				System.out.println("Neetworj error 1 ************8 ");
+		          //txtResult.setText("Failed!");
+			}
+			
+		   }
+		 public  void Userrequest(HttpResponse response){
+		    
+		    	try{
+		    	        InputStream in = response.getEntity().getContent();
+		    	        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		    	        StringBuilder str = new StringBuilder();
+		    	        String line = null;
+		    	        while((line = reader.readLine()) != null){
+		    	            str.append(line + "\n");
+		    	        }
+		    	        in.close();
+		    	        result = str.toString();
+		    	        updateData(result);
+		    	   }catch(Exception ex){
+		    		   System.out.println("Neetworj error 2 ************8 ");
+		    	        result = "Error";
+		    	   }
+		    	    
+    	   }
+		 public  void updateData(String result)
+		    {
+		   	 try
+		        {
+		         JSONObject json=new JSONObject(result);
+		         JSONArray ja;
+		         ja = json.getJSONArray("routes");
+		         json = ja.getJSONObject(1);
+		         ja = json.getJSONArray("legs");
+		         json = ja.getJSONObject(0);
+		         ja = json.getJSONArray("steps");
+		         int resultCount = ja.length();
+		         System.out.println("REsult steps is "+ja.toString());
+		         
+		         for (int i = 0; i < resultCount; i++)
+		           {
+		           JSONObject resultObject = ja.getJSONObject(i);
+		           System.out.println("REsult steps is "+resultObject.toString());
+		           }
+		        }
+		         catch(Exception e)
+		         {
+		         	
+		         System.out.println("Exception is "+e.toString());
+		         }
+		    }
+}
--- trunk/src/com/android/localguide/MapsActivity.java
+++ trunk/src/com/android/localguide/MapsActivity.java
@@ -1,15 +1,65 @@
 package com.android.localguide;
 
-import android.os.Bundle;
+import java.util.List;
 
-public class MapsActivity extends MapsActivity{
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ZoomControls;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapsActivity extends MapActivity{
+
+	private MapController myMapController;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.mapsview);
 	    MapView mapView = (MapView) findViewById(R.id.mapview);
+
+	    ZoomControls zoomControls = (ZoomControls) mapView.getZoomControls();
+	    zoomControls.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT,
+	    LayoutParams.WRAP_CONTENT));
+
+	    mapView.addView(zoomControls);
+	    
 	    mapView.setBuiltInZoomControls(true);
+	   
+	    List<Overlay> mapOverlays = mapView.getOverlays();
+	    mapOverlays.clear();
+	    
+	    Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+	    
+	    MapsItemizedOverlay itemizedoverlay = new MapsItemizedOverlay(drawable,this);
+	   
+	    GeoPoint point = new GeoPoint(17375812,78490667);
+	    OverlayItem overlayitem = new OverlayItem(point, "Laissez les bon temps rouler!", "I'm in Louisiana!");
+
+	    GeoPoint point2 = new GeoPoint(17385812,78480667);
+	    OverlayItem overlayitem2 = new OverlayItem(point2, "Namashkaar!", "I'm in Hyderabad, India!");
+
+	    GeoPoint point3 = new GeoPoint(17365812,78460667);
+	    OverlayItem overlayitem3 = new OverlayItem(point3, "Manikandan!","I am Mani");
+
+	    
+	    itemizedoverlay.addOverlay(overlayitem);
+	    itemizedoverlay.addOverlay(overlayitem2);
+	    itemizedoverlay.addOverlay(overlayitem3);
+	    
+	    mapOverlays.add(itemizedoverlay);
+		mapView.invalidate();
+	    
+		myMapController = mapView.getController();
+	    myMapController.animateTo(point2);
+	    myMapController.setZoom(13); //Fixed Zoom Level
 	}
 	
 	protected boolean isRouteDisplayed() {
