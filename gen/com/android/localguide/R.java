7e105cf4944dae19583d75b13a96ce85fd2b0f42
smanikandan14 <smanikandan14@a7035fdc-282f-eb65-315d-6cc58e1ab02f>
2011-05-08 06:06:21 +0000

    Added layout animations to the widget. And changed the font sizes of title, address,phonumber, added phone,message icons to directly make calls and send sms from widget. Changed the color codes and drawables for buttons.

--- gen/com/android/localguide/R.java
+++ gen/com/android/localguide/R.java
@@ -10,7 +10,8 @@ package com.android.localguide;
 public final class R {
     public static final class anim {
         public static final int fade_in=0x7f040000;
-        public static final int layout_anim=0x7f040001;
+        public static final int from_right=0x7f040001;
+        public static final int layout_anim=0x7f040002;
     }
     public static final class attr {
     }
@@ -29,25 +30,30 @@ public final class R {
         public static final int list_normal=0x7f02000b;
         public static final int list_pressed=0x7f02000c;
         public static final int localguide=0x7f02000d;
-        public static final int next_arrow=0x7f02000e;
-        public static final int police=0x7f02000f;
-        public static final int previous_arrow=0x7f020010;
-        public static final int rowselector=0x7f020011;
-        public static final int shopping=0x7f020012;
-        public static final int spinner_white_48=0x7f020013;
-        public static final int taxi=0x7f020014;
-        public static final int telephone=0x7f020015;
-        public static final int theatre=0x7f020016;
-        public static final int train=0x7f020017;
-        public static final int ubuntu=0x7f020018;
-        public static final int widgetbackground=0x7f020019;
-        public static final int widgetbackground1=0x7f02001a;
+        public static final int message=0x7f02000e;
+        public static final int next_arrow=0x7f02000f;
+        public static final int phone=0x7f020010;
+        public static final int police=0x7f020011;
+        public static final int previous_arrow=0x7f020012;
+        public static final int rowselector=0x7f020013;
+        public static final int shopping=0x7f020014;
+        public static final int spinner_white_48=0x7f020015;
+        public static final int taxi=0x7f020016;
+        public static final int telephone=0x7f020017;
+        public static final int theatre=0x7f020018;
+        public static final int train=0x7f020019;
+        public static final int ubuntu=0x7f02001a;
+        public static final int widget_button_off=0x7f02001b;
+        public static final int widget_button_on=0x7f02001c;
+        public static final int widgetbackground=0x7f02001d;
+        public static final int widgetbackground1=0x7f02001e;
+        public static final int widgetbuttonpress=0x7f02001f;
     }
     public static final class id {
         public static final int MainLayout=0x7f07000e;
         public static final int address=0x7f070016;
         public static final int addressLayout=0x7f07000c;
-        public static final int button=0x7f070032;
+        public static final int button=0x7f070034;
         public static final int button1=0x7f07001b;
         public static final int button2=0x7f07001c;
         public static final int button3=0x7f07001d;
@@ -55,6 +61,7 @@ public final class R {
         public static final int button5=0x7f07001f;
         public static final int button6=0x7f070020;
         public static final int button7=0x7f070021;
+        public static final int call=0x7f070032;
         public static final int call_list=0x7f070003;
         public static final int categories=0x7f07002a;
         public static final int categories_list=0x7f07002f;
@@ -78,6 +85,7 @@ public final class R {
         public static final int locationtextbox=0x7f07002b;
         public static final int logo=0x7f070024;
         public static final int mapview=0x7f070017;
+        public static final int message=0x7f070033;
         public static final int morebutton=0x7f070023;
         public static final int myTableLayout=0x7f07001a;
         public static final int myTableLayout1=0x7f07000a;
@@ -117,6 +125,7 @@ public final class R {
         public static final int widgetconfigurelistview=0x7f030011;
         public static final int widgetlayout=0x7f030012;
         public static final int widgetlayout1=0x7f030013;
+        public static final int widgetlayout3=0x7f030014;
     }
     public static final class string {
         public static final int app_name=0x7f060001;
