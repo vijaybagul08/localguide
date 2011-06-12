6b46caf9c78040425c834966702b37adb7d6f484
smanikandan14 <smanikandan14@a7035fdc-282f-eb65-315d-6cc58e1ab02f>
2011-06-12 14:53:47 +0000

    Added getdirections dialog to get the from location from user before searching for routes.

--- trunk/gen/com/android/localguide/R.java
+++ trunk/gen/com/android/localguide/R.java
@@ -18,10 +18,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int app_background_color=0x7f070000;
-        public static final int edittext_bkg=0x7f070001;
-        public static final int list_elements_bkg=0x7f070002;
-        public static final int tab_divider=0x7f070003;
+        public static final int app_background_color=0x7f080000;
+        public static final int edittext_bkg=0x7f080001;
+        public static final int list_elements_bkg=0x7f080002;
+        public static final int tab_divider=0x7f080003;
     }
     public static final class drawable {
         public static final int app_bkg_color=0x7f02002a;
@@ -70,59 +70,61 @@ public final class R {
         public static final int widgetbuttonpress=0x7f020029;
     }
     public static final class id {
-        public static final int MainLayout=0x7f08000e;
-        public static final int address=0x7f080016;
-        public static final int addressLayout=0x7f08000c;
-        public static final int button=0x7f080034;
-        public static final int button1=0x7f08001b;
-        public static final int button2=0x7f08001c;
-        public static final int button3=0x7f08001d;
-        public static final int button4=0x7f08001e;
-        public static final int button5=0x7f08001f;
-        public static final int button6=0x7f080020;
-        public static final int button7=0x7f080021;
-        public static final int call=0x7f080032;
-        public static final int call_list=0x7f080003;
-        public static final int categories=0x7f08002a;
-        public static final int categories_list=0x7f08002f;
-        public static final int category=0x7f08002e;
-        public static final int categoryIcon=0x7f080030;
-        public static final int categoryText=0x7f080006;
-        public static final int categoryimage=0x7f080005;
-        public static final int categotytextbox=0x7f080028;
-        public static final int checkbox=0x7f08002c;
-        public static final int close=0x7f080001;
-        public static final int description=0x7f08000f;
-        public static final int description1=0x7f080010;
-        public static final int facebook=0x7f080014;
-        public static final int facebooktext=0x7f080013;
-        public static final int gridview=0x7f080008;
-        public static final int hint=0x7f080026;
-        public static final int layout_call=0x7f080000;
-        public static final int layout_configure=0x7f08002d;
-        public static final int layout_root=0x7f080007;
-        public static final int list=0x7f080009;
-        public static final int locationtextbox=0x7f08002b;
-        public static final int logo=0x7f080024;
-        public static final int mapview=0x7f080017;
-        public static final int message=0x7f080033;
-        public static final int morebutton=0x7f080023;
-        public static final int myTableLayout=0x7f08001a;
-        public static final int myTableLayout1=0x7f08000a;
-        public static final int next=0x7f08000d;
-        public static final int phonenumber=0x7f080004;
-        public static final int previous=0x7f08000b;
-        public static final int resultsLayout=0x7f080022;
-        public static final int search=0x7f080029;
-        public static final int spinner=0x7f080018;
-        public static final int text=0x7f080019;
-        public static final int text1=0x7f080002;
-        public static final int title=0x7f080015;
-        public static final int twitter=0x7f080012;
-        public static final int twitterLayout=0x7f080025;
-        public static final int twitter_message_edit=0x7f080027;
-        public static final int twittertext=0x7f080011;
-        public static final int widgetlayout=0x7f080031;
+        public static final int MainLayout=0x7f09000e;
+        public static final int address=0x7f090016;
+        public static final int addressLayout=0x7f09000c;
+        public static final int button=0x7f090036;
+        public static final int button1=0x7f09001f;
+        public static final int button2=0x7f090020;
+        public static final int button3=0x7f090021;
+        public static final int button4=0x7f090022;
+        public static final int button5=0x7f090023;
+        public static final int button6=0x7f090024;
+        public static final int button7=0x7f090025;
+        public static final int call=0x7f090034;
+        public static final int call_list=0x7f090003;
+        public static final int cancel=0x7f09001a;
+        public static final int categories=0x7f09002e;
+        public static final int categories_list=0x7f090031;
+        public static final int category=0x7f090030;
+        public static final int categoryIcon=0x7f090032;
+        public static final int categoryText=0x7f090006;
+        public static final int categoryimage=0x7f090005;
+        public static final int categotytextbox=0x7f09002c;
+        public static final int checkbox=0x7f090017;
+        public static final int close=0x7f090001;
+        public static final int description=0x7f09000f;
+        public static final int description1=0x7f090010;
+        public static final int facebook=0x7f090014;
+        public static final int facebooktext=0x7f090013;
+        public static final int gridview=0x7f090008;
+        public static final int hint=0x7f09002a;
+        public static final int layout_call=0x7f090000;
+        public static final int layout_configure=0x7f09002f;
+        public static final int layout_root=0x7f090007;
+        public static final int list=0x7f090009;
+        public static final int locationtextbox=0x7f090018;
+        public static final int logo=0x7f090028;
+        public static final int mapview=0x7f09001b;
+        public static final int message=0x7f090035;
+        public static final int morebutton=0x7f090027;
+        public static final int myTableLayout=0x7f09001e;
+        public static final int myTableLayout1=0x7f09000a;
+        public static final int next=0x7f09000d;
+        public static final int ok=0x7f090019;
+        public static final int phonenumber=0x7f090004;
+        public static final int previous=0x7f09000b;
+        public static final int resultsLayout=0x7f090026;
+        public static final int search=0x7f09002d;
+        public static final int spinner=0x7f09001c;
+        public static final int text=0x7f09001d;
+        public static final int text1=0x7f090002;
+        public static final int title=0x7f090015;
+        public static final int twitter=0x7f090012;
+        public static final int twitterLayout=0x7f090029;
+        public static final int twitter_message_edit=0x7f09002b;
+        public static final int twittertext=0x7f090011;
+        public static final int widgetlayout=0x7f090033;
     }
     public static final class layout {
         public static final int calldialog=0x7f030000;
@@ -134,18 +136,19 @@ public final class R {
         public static final int information=0x7f030006;
         public static final int listview=0x7f030007;
         public static final int maintab=0x7f030008;
-        public static final int mapsview=0x7f030009;
-        public static final int myprogressdialog=0x7f03000a;
-        public static final int options=0x7f03000b;
-        public static final int results=0x7f03000c;
-        public static final int splash=0x7f03000d;
-        public static final int twitterlayout=0x7f03000e;
-        public static final int welcome=0x7f03000f;
-        public static final int widgetconfigure=0x7f030010;
-        public static final int widgetconfigurelistview=0x7f030011;
-        public static final int widgetlayout=0x7f030012;
-        public static final int widgetlayout1=0x7f030013;
-        public static final int widgetlayout3=0x7f030014;
+        public static final int maps_options_dialog=0x7f030009;
+        public static final int mapsview=0x7f03000a;
+        public static final int myprogressdialog=0x7f03000b;
+        public static final int options=0x7f03000c;
+        public static final int results=0x7f03000d;
+        public static final int splash=0x7f03000e;
+        public static final int twitterlayout=0x7f03000f;
+        public static final int welcome=0x7f030010;
+        public static final int widgetconfigure=0x7f030011;
+        public static final int widgetconfigurelistview=0x7f030012;
+        public static final int widgetlayout=0x7f030013;
+        public static final int widgetlayout1=0x7f030014;
+        public static final int widgetlayout3=0x7f030015;
     }
     public static final class string {
         public static final int app_name=0x7f060001;
@@ -154,6 +157,9 @@ public final class R {
         public static final int description1=0x7f060003;
         public static final int hello=0x7f060000;
         public static final int route_error=0x7f060005;
+    }
+    public static final class style {
+        public static final int getdirectionsdialog=0x7f070000;
     }
     public static final class xml {
         public static final int widgetprovider=0x7f050000;
---
+++ trunk/src/com/android/localguide/GetDirectionsDialog.java
@@ -0,0 +1,154 @@
+package com.android.localguide;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+public class GetDirectionsDialog extends Dialog implements OnClickListener {
+
+    interface GetDirectionsDialogListener {
+    	public void onButtonOkPressed(boolean isCurrentLocation,String location);
+    }
+
+	Context mContext;
+	Dialog dialog;
+	Typeface fontface;
+	int screenWidth;
+	CheckBox locationCheckbox;
+	EditText locationTextbox;
+	boolean isLocationChkBoxChecked;
+	Button mOK;
+	Button mCancel;
+	GetDirectionsDialogListener mCB;
+	public GetDirectionsDialog(Context context,GetDirectionsDialogListener aCB )
+	{
+
+		super(context, R.style.getdirectionsdialog);	
+		mCB = aCB;
+		mContext = context;
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.maps_options_dialog);
+		Display display = ((WindowManager)mContext.getSystemService(mContext.WINDOW_SERVICE)).getDefaultDisplay();  
+		isLocationChkBoxChecked = true;
+		screenWidth = display.getWidth();  
+		int height = display.getHeight();
+		mOK = (Button) findViewById(R.id.ok);
+		mCancel = (Button) findViewById(R.id.cancel);
+		locationTextbox = (EditText)findViewById(R.id.locationtextbox);
+		locationTextbox.setText("");
+		locationCheckbox =(CheckBox)findViewById(R.id.checkbox);
+		locationCheckbox.setOnClickListener(new CheckBox.OnClickListener(){
+		    	 public void onClick(View v) {   
+		               if(((CheckBox)v).isChecked())
+		               {
+		            	   isLocationChkBoxChecked = true;
+		            	   TextView text1 = (TextView)findViewById(R.id.text1);
+		            	   text1.setVisibility(View.GONE);
+		            	   locationTextbox.setVisibility(View.GONE);
+		               }
+		               else
+		               { 
+		               	   isLocationChkBoxChecked = false;
+		                   TextView text1 = (TextView)findViewById(R.id.text1);
+		            	   text1.setVisibility(View.VISIBLE);
+		            	   locationTextbox.setVisibility(View.VISIBLE);
+		               }
+		    	 	}   
+		     });
+ 		
+ 			mOK.setOnClickListener(new Button.OnClickListener(){
+	            public void onClick(View v) {
+
+					if(locationCheckbox.isChecked() == true)
+					{
+						mCB.onButtonOkPressed(true,null);
+						GetDirectionsDialog.this.dismiss();
+					}
+					else
+					{
+					System.out.println("Dialog on click ***************** 2");
+						if(locationTextbox.getText().toString().length() > 0)
+						{
+							mCB.onButtonOkPressed(false,locationTextbox.getText().toString());
+							GetDirectionsDialog.this.dismiss();
+						}
+						else
+						{
+						System.out.println("Dialog on click ***************** 3 ");
+				    		AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+				    		alertDialog.setTitle("Error");
+				    		alertDialog.setMessage("Please enter a valid from location");
+				    		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				    		      public void onClick(DialogInterface dialog, int which) {
+				    		 
+				    		       //here you can add functions
+				    		 
+				    		    } });
+				    		alertDialog.setIcon(R.drawable.icon);
+				    		alertDialog.show();
+						}
+					}
+	            }
+	         });
+
+ 			mCancel.setOnClickListener(new Button.OnClickListener(){
+	            public void onClick(View v) {
+	            	GetDirectionsDialog.this.dismiss();
+	            }
+	         });
+
+	         
+		}
+	
+		public void show()
+		{
+			super.show();
+		    WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+		    lp.width = screenWidth-10;
+		    this.getWindow().setAttributes(lp);
+	    }		
+	    
+		public void onClick(View v) {
+		System.out.println("Dialog on click ***************** ");
+			if(v == mCancel )
+				this.dismiss();
+			else if(v == mOK)
+			{
+			System.out.println("Dialog on click ***************** 11");
+				if(locationCheckbox.isChecked() == true)
+					mCB.onButtonOkPressed(true,null);
+				else
+				{
+				System.out.println("Dialog on click ***************** 2");
+					if(locationTextbox.getText().toString().length() > 0)
+						mCB.onButtonOkPressed(false,locationTextbox.getText().toString());
+					else
+					{
+					System.out.println("Dialog on click ***************** 3 ");
+			    		AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+			    		alertDialog.setTitle("Error");
+			    		alertDialog.setMessage("Please enter a valid from location");
+			    		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			    		      public void onClick(DialogInterface dialog, int which) {
+			    		 
+			    		       //here you can add functions
+			    		 
+			    		    } });
+			    		alertDialog.setIcon(R.drawable.icon);
+			    		alertDialog.show();
+					}
+				}
+			}
+		}
+}
